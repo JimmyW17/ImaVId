1df283af4e4caa6e04b6ed0336836bd10112498a
Jimmy Wu <wujimmy94@gmail.com>
2017-05-30 22:36:04 -0400

    fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,14 @@
 class HomeController < ApplicationController
   def index
     @haspictures = false;
-    if user_signed_in? && current_user.album.pictures && current_user.album.pictures.size > 0
-      @album = current_user.album
-      @pictures = @album.pictures
-      @picture = @pictures[rand(0..@pictures.size-1)]
-      @rand_img = @picture.picture_remote_url
-      @haspictures = true;
+    if user_signed_in?
+      if current_user.album.pictures && current_user.album.pictures.size > 0
+        @album = current_user.album
+        @pictures = @album.pictures
+        @picture = @pictures[rand(0..@pictures.size-1)]
+        @rand_img = @picture.picture_remote_url
+        @haspictures = true;
+      end
     end
   end
 end
