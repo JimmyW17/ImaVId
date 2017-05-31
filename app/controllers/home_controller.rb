925ab4d5786a27b75cd0e4667fe94f6c94d2b99a
Jimmy Wu <wujimmy94@gmail.com>
2017-05-30 22:25:42 -0400

    fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    if user_signed_in? && current_user.album.pictures.size > 0
+    if user_signed_in? && current_user.album.pictures && current_user.album.pictures.size > 0
       @album = current_user.album
       @pictures = @album.pictures
       @picture = @pictures[rand(0..@pictures.size-1)]
